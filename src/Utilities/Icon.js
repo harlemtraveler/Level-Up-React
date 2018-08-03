961c181edc5e7295438fb6d7a041e4c64f559602
Tim <tjk315@gmail.com>
2018-08-03 01:28:01 -0400

    Add close-button icon && index file for Utilities

---
+++ src/Utilities/Icon.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+
+class Icon extends Component {
+  static defaultProps = {
+    color: 'black'
+  }
+
+  render() {
+    switch (this.props.name) {
+      case 'close':
+        return (
+          <svg
+            x="0px"
+            y="0px"
+            width="24px"
+            height="24px"
+            viewBox="0 0 512 512"
+            enableBackground="new 0 0 512 512"
+          >
+            <path
+              fill={this.props.color}
+              d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm0 394c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h404c3.3 0 6 2.7 6 6v340zM356.5 194.6L295.1 256l61.4 61.4c4.6 4.6 4.6 12.1 0 16.8l-22.3 22.3c-4.6 4.6-12.1 4.6-16.8 0L256 295.1l-61.4 61.4c-4.6 4.6-12.1 4.6-16.8 0l-22.3-22.3c-4.6-4.6-4.6-12.1 0-16.8l61.4-61.4-61.4-61.4c-4.6-4.6-4.6-12.1 0-16.8l22.3-22.3c4.6-4.6 12.1-4.6 16.8 0l61.4 61.4 61.4-61.4c4.6-4.6 12.1-4.6 16.8 0l22.3 22.3c4.7 4.6 4.7 12.1 0 16.8z"
+            >
+            </path>
+          </svg>
+        )
+    }
+  }
+
+}
+
+export default Icon;
+
+/*
+  To improve the look of the close button svg icon,
+  New style properties were added to the CloseButton
+  Styled Component:
+
+    - border: none;
+    - background: transparent;
+    - padding: 10px;
+*/
+
+/*
+Window-Close Icon from Level Up Tutorials:
+
+<svg
+  x="0px"
+  y="0px"
+  width="24px"
+  height="24px"
+  viewBox="0 0 31.11 31.11"
+  enableBackground="new 0 0 31.11 31.11"
+>
+  <polygon
+    fill={this.props.color}
+    points="31.11,1.41 29.7,0 15.56,14.14 1.41,0 0,1.41 14.14,15.56 0,29.7 1.41,31.11 15.56,16.97   29.7,31.11 31.11,29.7 16.97,15.56 "
+  />
+</svg>
+*/
+
+/*
+Window-Close Icon from Font Awesome
+
+<svg
+  aria-hidden="true"
+  data-prefix="far"
+  data-icon="window-close"
+  class="svg-inline--fa fa-window-close fa-w-16"
+  role="img" xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 512 512"
+>
+  <path
+    fill="currentColor"
+    d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm0 394c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h404c3.3 0 6 2.7 6 6v340zM356.5 194.6L295.1 256l61.4 61.4c4.6 4.6 4.6 12.1 0 16.8l-22.3 22.3c-4.6 4.6-12.1 4.6-16.8 0L256 295.1l-61.4 61.4c-4.6 4.6-12.1 4.6-16.8 0l-22.3-22.3c-4.6-4.6-4.6-12.1 0-16.8l61.4-61.4-61.4-61.4c-4.6-4.6-4.6-12.1 0-16.8l22.3-22.3c4.6-4.6 12.1-4.6 16.8 0l61.4 61.4 61.4-61.4c4.6-4.6 12.1-4.6 16.8 0l22.3 22.3c4.7 4.6 4.7 12.1 0 16.8z"
+  >
+  </path>
+</svg>
+*/
+
+/*
+Modified Font Awesome icon:
+
+<svg
+  x="0px"
+  y="0px"
+  width="24px"
+  height="24px"
+  viewBox="0 0 512 512"
+  enableBackground="new 0 0 31.11 31.11"
+>
+  <path
+    fill={this.props.color}
+    d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm0 394c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h404c3.3 0 6 2.7 6 6v340zM356.5 194.6L295.1 256l61.4 61.4c4.6 4.6 4.6 12.1 0 16.8l-22.3 22.3c-4.6 4.6-12.1 4.6-16.8 0L256 295.1l-61.4 61.4c-4.6 4.6-12.1 4.6-16.8 0l-22.3-22.3c-4.6-4.6-4.6-12.1 0-16.8l61.4-61.4-61.4-61.4c-4.6-4.6-4.6-12.1 0-16.8l22.3-22.3c4.6-4.6 12.1-4.6 16.8 0l61.4 61.4 61.4-61.4c4.6-4.6 12.1-4.6 16.8 0l22.3 22.3c4.7 4.6 4.7 12.1 0 16.8z"
+  >
+  </path>
+</svg>
+*/
---
+++ src/Utilities/index.js
@@ -0,0 +1,8 @@
+// import * from './Filename'
+
+import Icon from './Icon';
+import Modal from './Modal';
+import Portal from './Portal';
+import Toggle from './Toggle';
+
+export { Icon, Modal, Portal, Toggle };
