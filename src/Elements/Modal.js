e2e4cf01599c5664f8351d28c282ab5e7eafd844
Tim <tjk315@gmail.com>
2018-08-03 15:44:18 -0400

    Import Card Styled-Comp. into Modal.js ModalCard Styled-Comp.

--- src/Elements/Modal.js
+++ src/Elements/Modal.js
@@ -3,7 +3,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Portal } from 'Utilities';
-import { Icon, Card } from 'Elements';
+// import { Icon, Card } from 'Elements';
+import Icon from './Icon';
+import { Card } from './Cards';
 
 
 class Modal extends Component {
@@ -39,14 +41,10 @@ const ModalWrapper = styled.div`
   align-items: center;
 `;
 
-const ModalCard = styled.div`
+const ModalCard = Card.extend`
   position: relative;
-  background: white;
-  border-radius: 5px;
-  padding: 15px;
   min-width: 320px;
   z-index: 10;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
   margin-bottom: 250px;
 `;
 
