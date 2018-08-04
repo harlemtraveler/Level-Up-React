d32f29cbaf0e3958f806ea9de8f475af1c6d28f5
Tim <tjk315@gmail.com>
2018-08-04 01:19:12 -0400

    Add 'native' to Transition element & utilized 'interpolate'

--- src/Elements/Modal.js
+++ src/Elements/Modal.js
@@ -2,7 +2,7 @@
 // not including './' before an import's source path.
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Transition } from 'react-spring';
+import { Transition, animated, config } from 'react-spring';
 import { Portal, absolute } from 'Utilities';
 // import { Icon, Card } from 'Elements';
 import Icon from './Icon';
@@ -16,24 +16,34 @@ class Modal extends Component {
     return (
       <Portal>
         <Transition
-          from={{ opacity: 0, bgOpacity: 0, y: -50 }}
-          enter={{ opacity: 1, bgOpacity: 0.5, y: 0 }}
-          leave={{ opacity: 0, bgOpacity: 0, y: 50 }}
+          // native is used to solve performance issues. It's also required
+          // for interpolate to work.
+          native
+          from={{ opacity: 0, bgOpacity: 0, y: '-50px' }}
+          enter={{ opacity: 1, bgOpacity: 0.5, y: '0px' }}
+          leave={{ opacity: 0, bgOpacity: 0, y: '50px' }}
         >
-          { on && (
-            (styles) => (
+          {on &&
+            (styles => (
               <ModalWrapper>
-                <ModalCard style={{
-                  transform: `translate3d(0, ${styles.y}px, 0)`,
-                  ...styles
-                }}>
+                <ModalCard
+                  style={{
+                    transform: styles.y.interpolate(
+                      y => `translate3d(0, ${y}, 0)`
+                    ),
+                    ...styles
+                  }}>
                   <CloseButton onClick={toggle}>
                     <Icon name="close" />
                   </CloseButton>
                   <div>{children}</div>
                 </ModalCard>
                 <Background
-                  style={{ opacity: styles.bgOpacity }}
+                  style={{
+                    opacity: styles.bgOpacity.interpolate(
+                      bgOpacity => bgOpacity
+                    )
+                  }}
                   onClick={toggle}
                 />
               </ModalWrapper>
@@ -53,7 +63,11 @@ const ModalWrapper = styled.div`
   align-items: center;
 `;
 
-const ModalCard = Card.extend`
+// This will adapt all styles from Card, but use 'animated.div' component.
+const AnimCard = Card.withComponent(animated.div);
+
+// This will use AnimCard to use Card style with react-spring's 'animated'.
+const ModalCard = AnimCard.extend`
   position: relative;
   min-width: 320px;
   z-index: 10;
@@ -70,7 +84,7 @@ const CloseButton = styled.button`
     })};
 `;
 
-const Background = styled.div`
+const Background = styled(animated.div)`
   ${absolute({})};
   width: 100%;
   height: 100%;
@@ -78,4 +92,31 @@ const Background = styled.div`
   opacity: 0.5;
 `;
 
+/*
+  To extend a component that exist, use 'styled()' and pass it
+  the existing component with a '.div' (or whatever element type)
+
+    const myStyledComp = styled(animated.div)`
+      myProperty: myValue;
+    `;
+*/
 export default Modal;
+// interpolate is a func that has callback func with a value
+
+/*
+  This is the code for ModalCard used in the tutorial. However, interpolate
+  doesn't work for me at all.
+  ******SOLVED*****
+  - The Transition component needed the 'native' property for interpolate to
+    work.
+
+    <ModalCard
+      style={{
+        transform: styles.y.interpolate(
+          y => `translate3d(0, ${y}, 0)`
+        ),
+        ...styles
+      }}>
+
+
+*/
