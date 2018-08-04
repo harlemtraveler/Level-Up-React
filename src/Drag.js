a14e36e9b09677f0d0bb73b428bcac320be4deda
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-08-04 15:21:07 -0400

    Initialized Gesture element

--- src/Drag.js
+++ src/Drag.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
+import { Gesture } from 'react-with-gesture';
 import { Card } from 'Elements';
 
 class Drag extends Component {
 
   render() {
     return (
-      <Draggable>
-        <Card>
-          <h1>Drag</h1>
-        </Card>
-      </Draggable>
+      // Gesture tag won't work until the input of dep. coordinates.
+      // The UI will not render until input of dependent coordinates.
+      <Gesture>
+        {/* Below are coordinates require for Gesture to work */}
+        {({ down, x, y, xDelta, yDelta, xInitial, yInitial }) => (
+          <Card>
+            <h1>
+              {down}, {x}, {y}, {xDelta}, {yDelta}, {xInitial}, {yInitial}
+            </h1>
+          </Card>
+        )}
+      </Gesture>
     );
   }
 
